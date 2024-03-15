a47c0912f758448d8833324194e6cd7d62da11f3
Wazeer Ahmed <wazeer@hashinclude.io>
2024-03-15 13:14:04 +0530

    close rotation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import HeroSect from './components/HeroSect';
-import Layout from './components/Layout';
+import logo from "./logo.svg";
+import "./App.css";
+import HeroSect from "./components/HeroSect";
+import Layout from "./components/Layout";
+import Close from "./assets/close.png";
 
 function App() {
   return (
-    <div className='bg-black text-white'>
-      <Layout/>
+    <div className="bg-black text-white">
+      <div className="flex justify-end px-10">
+        <div className="pt-4 hover:rotate-180 transform-origin-center transition-transform duration-500 cursor-pointer">
+          <img src={Close} />
+        </div>
+      </div>
+      <Layout />
     </div>
   );
 }
